41edae20ce49d51806e4e23d5ed25abee6e2fe6f
yanakunn <nurarissadayana@graduate.utm.my>
2023-06-17 04:05:11 +0800

    added product display

---
+++ project/submission/Regex/regex_mongodb/.venv/lib/python3.11/site-packages/admin_soft/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView, PasswordResetConfirmView
+from admin_soft.forms import RegistrationForm, LoginForm, UserPasswordResetForm, UserSetPasswordForm, UserPasswordChangeForm
+from django.contrib.auth import logout
+
+# Create your views here.
+
+# Pages
+def index(request):
+
+  return render(request, 'pages/index.html', { 'segment': 'index' })
+
+def billing(request):
+  return render(request, 'pages/billing.html', { 'segment': 'billing' })
+
+def tables(request):
+  return render(request, 'pages/tables.html', { 'segment': 'tables' })
+
+def vr(request):
+  return render(request, 'pages/virtual-reality.html', { 'segment': 'vr' })
+
+def rtl(request):
+  return render(request, 'pages/rtl.html', { 'segment': 'rtl' })
+
+def profile(request):
+  return render(request, 'pages/profile.html', { 'segment': 'profile' })
+
+
+# Authentication
+class UserLoginView(LoginView):
+  template_name = 'accounts/login.html'
+  form_class = LoginForm
+
+def register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print('Account created successfully!')
+      return redirect('/accounts/login/')
+    else:
+      print("Register failed!")
+  else:
+    form = RegistrationForm()
+
+  context = { 'form': form }
+  return render(request, 'accounts/register.html', context)
+
+def logout_view(request):
+  logout(request)
+  return redirect('/accounts/login/')
+
+class UserPasswordResetView(PasswordResetView):
+  template_name = 'accounts/password_reset.html'
+  form_class = UserPasswordResetForm
+
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
+  template_name = 'accounts/password_reset_confirm.html'
+  form_class = UserSetPasswordForm
+
+class UserPasswordChangeView(PasswordChangeView):
+  template_name = 'accounts/password_change.html'
+  form_class = UserPasswordChangeForm
---
+++ project/submission/Regex/regex_mysql/CHANGELOG.md
@@ -0,0 +1,39 @@
+# Change Log
+
+## [1.0.3] 2023-04-03
+### Changes
+
+- Bump Design: [Django Admin Soft](https://github.com/app-generator/django-admin-soft-dashboard) `v1.0.11`
+
+## [1.0.2] 2023-01-29
+### Changes
+
+- Bump Design: [Django Admin Soft](https://github.com/app-generator/django-admin-soft-dashboard) `v1.0.8`
+- DOCS Update (readme). New sections:
+  - `How to customize the theme`
+  - Render deployment
+- Configure the project to use `home/templates`
+- Added `custom_footer` sample
+
+## [1.0.1] 2023-01-27
+### Changes
+
+- Updated Codebase
+- Latest **[Django](https://appseed.us/admin-dashboards/django/)** Version
+- Theme-able UI (no more hardcoded inside the project)
+- Theme: [Django Admin Soft](https://github.com/app-generator/django-admin-soft-dashboard)
+  - Usable via `PIP`
+- CI/CD included via `Render`
+
+## [1.0.0] 2021-10-28
+### Initial Release
+
+- UI Kit: Soft UI Dashboard v1.0.1
+- Django Codebase Version [v2.0.4](https://github.com/app-generator/boilerplate-code-django-dashboard/releases)
+  - Dependencies update (all packages)
+    - Use Django==3.2.6 (latest stable version)
+  - Better Code formatting
+  - Improved Files organization
+  - Optimize imports
+  - Docker Scripts Update
+- Tooling: added scripts to recompile the SCSS files (via Gulp)

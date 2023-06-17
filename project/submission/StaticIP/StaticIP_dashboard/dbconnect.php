c0131ee5ad9ce0c2500a6cbc15fa07ae5d9942d6
Racquelmae <98173281+Racquelmae@users.noreply.github.com>
2023-06-17 21:22:06 +0800

    update

---
+++ project/submission/StaticIP/StaticIP_dashboard/dbconnect.php
@@ -0,0 +1,12 @@
+<?php 
+
+//set DB Parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dashboard";
+
+//Create connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+
+?>

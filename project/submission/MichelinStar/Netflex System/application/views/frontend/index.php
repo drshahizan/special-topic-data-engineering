2ec2586582ee6671c766576f2e32a90729a848ca
Eddie Wong Chung Pheng <95403713+Prowong01@users.noreply.github.com>
2023-06-18 01:19:23 +0800

    Delete project/submission/MichelinStar/Netflex System directory

--- project/submission/MichelinStar/Netflex System/application/views/frontend/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-$selected_theme	= $this->crud_model->get_active_theme();
-include $selected_theme . '/index.php';
-?>
--- project/submission/MichelinStar/Netflex System/application/views/install/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="main"
-	style="text-align: center;">
-	Need help?  <a href="http://support.creativeitem.com" target="_blank" style="text-decoration:underline;">Contact support</a>
-</footer>
